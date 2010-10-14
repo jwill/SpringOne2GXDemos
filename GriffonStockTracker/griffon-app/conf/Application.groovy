8122decaf09fbfada8434beb6302a86be51d76af
James Williams <james.l.williams@gmail.com>
2010-10-14 13:56:04 -0500

    Stock tracker app baseline commit.

---
+++ GriffonStockTracker/griffon-app/conf/Application.groovy
@@ -0,0 +1,19 @@
+application {
+    title = 'GriffonStockTracker'
+    startupGroups = ['GriffonStockTracker']
+
+    // Should Griffon exit when no Griffon created frames are showing?
+    autoShutdown = true
+
+    // If you want some non-standard application class, apply it here
+    //frameClass = 'javax.swing.JFrame'
+}
+mvcGroups {
+    // MVC Group for "GriffonStockTracker"
+    'GriffonStockTracker' {
+        model = 'griffonstocktracker.GriffonStockTrackerModel'
+        controller = 'griffonstocktracker.GriffonStockTrackerController'
+        view = 'griffonstocktracker.GriffonStockTrackerView'
+    }
+
+}
---
+++ GriffonStockTracker/griffon-app/models/griffonstocktracker/GriffonStockTrackerModel.groovy
@@ -0,0 +1,7 @@
+package griffonstocktracker
+
+import groovy.beans.Bindable
+
+class GriffonStockTrackerModel {
+   // @Bindable String propName
+}
